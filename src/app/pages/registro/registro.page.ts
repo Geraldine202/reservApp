352d94e5782ab1976531db176d8caf0fd5aec069
Geraldine202 <ge.inostrozam@duocuc.cl>
2024-08-27 12:51:05 -0400

    se agrega formControl a registro

--- src/app/pages/registro/registro.page.ts
+++ src/app/pages/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  //aqui podemos crear variables:
+  //NOMBRE: TIPO = VALOR;
+  //nombre = Clase(valor);
+  persona = new FormGroup({
+    rut: new FormControl('',[Validators.minLength(9),Validators.maxLength(10),Validators.required,Validators.pattern("[0,9]{7,8}-[0-9Kk]{1}")]),
+    nombre: new FormControl('',[Validators.minLength(3),Validators.required,Validators.pattern("[a-z]{3,5}")]),
+    fecha_nacimiento: new FormControl(),
+    genero: new FormControl(),
+  });
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  //podemos crear métodos:
+  //public/private registrar
+  registrar():void{
+    //VALIDACIONES? llamar un DAO? conversion?
+    console.log(this.persona.value);
+    alert("Registrado!");
+    this.router.navigate(['/login']);
+
   }
 
 }
